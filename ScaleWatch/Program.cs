e0144b776df60cc389c3c3d35d5358fccfbe09a4
kbullinger <bullinger.karen@gmail.com>
2019-07-05 13:57:12 -0700

    changes

--- ScaleWatch/Program.cs
+++ ScaleWatch/Program.cs
@@ -16,7 +16,7 @@ namespace ScaleWatch
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new ScaleWatchService()
             };
             ServiceBase.Run(ServicesToRun);
         }
